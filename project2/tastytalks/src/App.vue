509f14d39755df4d89bc4340a184ea36ac2eec32
Ian Boyd <ianbinnc@gmail.com>
2023-11-13 20:20:32 -0500

    Converted/merged into the current backend

---
+++ project2/tastytalks/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <TastyTalksHeader />
+    <div id="nav" v-if="$store.state.user">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <button @click="$store.dispatch('logout')">Logout</button>
+    </div>
+  <router-view/>
+  <TastyTalksFooter />
+</template>
+
+<script>
+import { onBeforeMount } from 'vue'
+import { useStore } from 'vuex'
+import TastyTalksHeader from './components/TastyTalksHeader.vue'
+import TastyTalksFooter from './components/TastyTalksFooter.vue'
+
+export default {
+    setup() {
+        const store = useStore();
+        onBeforeMount(() => {
+            store.dispatch("fetchUser");
+        });
+    },
+    components: { TastyTalksHeader, TastyTalksFooter }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
---
+++ project2/tastytalks/src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './assets/styles.css'; //Import the global css file
+createApp(App).use(store).use(router).mount('#app')
