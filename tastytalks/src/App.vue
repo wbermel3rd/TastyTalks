41f0c3f69f69583b76474227b93c602574d55b8d
wbermel3rd <willathrill@gmail.com>
2023-11-14 13:46:41 -0600

    Empty Vue project template
    Cleaning up. Await next commit, will contain necessary project files.

--- tastytalks/src/App.vue
+++ tastytalks/src/App.vue
@@ -1,32 +1,30 @@
-
-
-
 <template>
-  <div>
-    <TastyTalksHeader />
-    <router-view />
-    <TastyTalksFooter />
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-import TastyTalksHeader from './components/TastyTalksHeader.vue'
-import TastyTalksFooter from './components/TastyTalksFooter.vue'
-
-
-export default {
-  name: 'App',
-  components: {
-    TastyTalksHeader,
-    TastyTalksFooter
-}
-}
-</script>
 
 <style>
 #app {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- tastytalks/src/main.js
+++ tastytalks/src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/styles.css'; //Import the global css file
-import router from './router.js';
+import router from './router'
+import store from './store'
 
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+createApp(App).use(store).use(router).mount('#app')
