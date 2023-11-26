ab2720d837412e0dc60e40169af7124f92b87506
OliDeschamps <deschamps.ol@outlook.com>
2023-11-26 18:42:56 -0500

    Adding ideas for the home page + Ideas for the about us page

--- tastytalks/src/views/AboutView.vue
+++ tastytalks/src/views/AboutView.vue
@@ -1,29 +1,87 @@
 <template>
-  <main>
-    <div>
-      <h1>About Us</h1>
-      <div class="person-slider">
-        <button @click="prevPerson" class="nav-button">❮</button>
-        <img :src="person.image" :alt="person.name">
-        <h2>{{ person.name }}</h2>
-        <p>{{ person.jobTitle }}</p>
-        <button @click="nextPerson" class="nav-button">❯</button>
+
+  <div>
+    <h1>About Us</h1>
+    <div class="slider-container">
+      <div @click="prevPerson" class="nav-button left">❮</div>
+      <div class="slides">
+        <transition :name="slideDirection">
+          <div :key="currentPersonIndex" class="person-slider">
+            <img :src="persons[currentPersonIndex].image" :alt="persons[currentPersonIndex].name">
+            <h2>{{ persons[currentPersonIndex].name }}</h2>
+            <p>{{ persons[currentPersonIndex].jobTitle }}</p>
+          </div>
+        </transition>
       </div>
+      <div @click="nextPerson" class="nav-button right">❯</div>
     </div>
-  </main>
+  </div>
+
 </template>
 
 
 <script>
 
-
 export default {
-  props: ['person', 'prevPerson', 'nextPerson'],
+  data() {
+    return {
+      persons: [
+        { id: 1, name: 'John Doe', jobTitle: 'Job Title 1', image: 'person1.jpg' },
+        { id: 2, name: 'Jane Smith', jobTitle: 'Job Title 2', image: 'person2.jpg' },
+        // Add more persons as needed
+      ],
+      currentPersonIndex: 0,
+      slideDirection: '',
+    };
+  },
+  watch: {
+    currentPersonIndex(newValue, oldValue) {
+      this.slideDirection = newValue > oldValue ? 'slide-right' : 'slide-left';
+    },
+  },
+  methods: {
+    prevPerson() {
+      this.currentPersonIndex = (this.currentPersonIndex - 1 + this.persons.length) % this.persons.length;
+    },
+    nextPerson() {
+      this.currentPersonIndex = (this.currentPersonIndex + 1) % this.persons.length;
+    },
+  },
 };
 
 </script>
 
 <style>
+
+.slider-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 20px;
+}
+
+.nav-button {
+  font-size: 24px;
+  cursor: pointer;
+  background: none;
+  border: none;
+}
+
+.left {
+  transform: rotate(180deg);
+}
+
+.right {
+  transform: rotate(0deg);
+}
+
+.nav-button:hover {
+  color: #333;
+}
+
+.slides {
+  width: 80vw; /* 80% of viewport width */
+}
 
 .person-slider {
   position: relative;
@@ -42,18 +100,15 @@ export default {
   margin-bottom: 10px;
 }
 
-.nav-button {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  font-size: 24px;
-  background: none;
-  border: none;
-  cursor: pointer;
+.slide-left-enter-active, .slide-right-enter-active, .slide-left-leave-active, .slide-right-leave-active {
+  transition: transform 0.5s;
 }
 
-.nav-button:hover {
-  color: #333;
+.slide-left-enter, .slide-right-leave-to {
+  transform: translateX(-100%);
 }
 
+.slide-right-enter, .slide-left-leave-to {
+  transform: translateX(100%);
+}
 </style>
--- tastytalks/src/views/HomePage.vue
+++ tastytalks/src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-      <h1>Welcome to Taksty Talks</h1>
+      <!-- <h1>Welcome to Taksty Talks</h1>
 
       <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos quia, laborum ut natus sequi unde reiciendis nostrum. Obcaecati reiciendis odit asperiores, voluptates accusamus quibusdam excepturi ipsam quidem quas ad debitis.</P>
 
@@ -17,9 +17,40 @@
         <div class="AboutUsSquare">
 
         </div>
+      </div> -->
+
+      <div class="main-page">
+    <!-- Ingredients of the Day Section -->
+    <div class="section ingredients-section">
+      <h2>Ingredients of the Day</h2>
+      <ul>
+        <li>Ingredient 1</li>
+        <li>Ingredient 2</li>
+        <li>Ingredient 3</li>
+        <!-- Add more ingredients as needed -->
+      </ul>
+    </div>
+
+    <!-- Recommended Recipe Section -->
+    <div class="section recommended-recipe-section">
+      <h2>Recommended Recipe</h2>
+      <div class="recipe-card">
+        <img src="recipe-image.jpg" alt="Recipe Image">
+        <div class="recipe-info">
+          <h3>Delicious Recipe Title</h3>
+          <p>Description of the recommended recipe. Lorem ipsum dolor sit amet...</p>
+          <a href="#" class="btn">View Recipe</a>
+        </div>
       </div>
+    </div>
 
-
+    <!-- Creative Element Section -->
+    <div class="section creative-element-section">
+      <h2>Explore Something New</h2>
+      <p>Discover unique cooking techniques, kitchen hacks, and more!</p>
+      <!-- Your creative element goes here -->
+    </div>
+  </div>
 
 
 
@@ -34,5 +65,51 @@
 </script>
   
 <style scoped>
+
+.main-page {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.section {
+  margin-bottom: 40px;
+}
+
+.ingredients-section ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+.recipe-card {
+  display: flex;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  overflow: hidden;
+}
+
+.recipe-card img {
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+}
+
+.recipe-info {
+  padding: 10px;
+}
+
+.btn {
+  display: inline-block;
+  padding: 8px 12px;
+  background-color: #4caf50;
+  color: #fff;
+  text-decoration: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background-color: #45a049;
+}
 
 </style>
