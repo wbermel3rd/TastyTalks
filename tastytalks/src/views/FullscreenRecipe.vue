7f57023bedfd1b326122223e74a90c9b71bca890
Ian Boyd <ianbinnc@gmail.com>
2023-12-03 12:53:17 -0500

    Added Comment create a view frontend

--- tastytalks/src/views/FullscreenRecipe.vue
+++ tastytalks/src/views/FullscreenRecipe.vue
@@ -14,8 +14,24 @@
 
       <div class="comments-container">
         <h2 class="comments-header">Comments:</h2>
+        <!-- Comments Section -->
+        <div class="comments">
+          <!-- Display comments here -->
+          <div class="comment-card">
+            <p class="comment-author">John Doe</p>
+            <p class="comment-text">This is a sample comment to show how it will look on the page.</p>
+            <p class="comment-date">2023-12-03</p>
+          </div>
       </div>
+        <!-- Add Comment Button -->
+        <button @click="showCommentBox = !showCommentBox">Add Comment</button>
 
+        <!-- Comment Box Section -->
+        <div v-if="showCommentBox" class="comment-box">
+          <textarea v-model="newComment" placeholder="Write your comment here..."></textarea>
+          <button @click="submitComment">Submit</button>
+        </div>
+    </div>
     </main>
   </template>
   
@@ -46,11 +62,23 @@
           console.error("Post ID is undefined");
         }
       };
+      const showCommentBox = ref(false); // For toggling the comment box
+      const newComment = ref(''); // For storing new comment text
+
+      const submitComment = () => {
+        console.log('Comment submitted:', newComment.value);
+        // Here, you can add logic to send the comment to your backend or Firebase
+        newComment.value = ''; // Clear the comment box after submission
+        showCommentBox.value = false; // Hide the comment box after submission
+      };
   
       onMounted(fetchPost);
   
       return {
         post,
+        showCommentBox,
+        newComment,
+        submitComment,
       };
     }
   };
@@ -58,6 +86,13 @@
   
   
   <style scoped>
+
+  h1{
+    color: black;
+  }
+  h2{
+    font-size: 25px;
+  }
   .full-post-view {
     max-width: 800px;
     margin: 20px auto;
@@ -65,6 +100,7 @@
     border: 1px solid #ccc;
     border-radius: 8px;
     background-color: white;
+    min-height: 500px;
   }
   .comments-container{
     max-width: 800px;
@@ -73,6 +109,52 @@
     border: 1px solid #ccc;
     border-radius: 8px;
     background-color: white;
+  }
+  .comment-box textarea {
+    width: 100%; /* Full width of the container */
+    max-width: 100%; /* Max width restricted to the container width */
+    box-sizing: border-box; /* Include padding and border in the element's total width and height */
+    overflow: auto; /* Add scrollbars if content overflows */
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+
+  .comments-container button {
+    margin-top: 10px;
+    padding: 6px 12px;
+    background-color: rgb(104, 150, 56);
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .comment-box button:hover {
+    background-color: #0056b3;
+  }
+  /* Comment Card */
+  .comment-card {
+    background-color: white;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 15px;
+    margin-top: 20px;
+  }
+
+  .comment-author {
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  .comment-text {
+    margin-bottom: 10px;
+  }
+
+  .comment-date {
+    font-size: 0.8em;
+    color: #666;
   }
 
   </style>
